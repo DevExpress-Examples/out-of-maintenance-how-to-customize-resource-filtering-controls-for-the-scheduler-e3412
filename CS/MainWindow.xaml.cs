5e95ad524758e6c91579956ce500f9922b86d108
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+using DevExpress.Xpf.Scheduler;
+using DevExpress.XtraScheduler;
+
+namespace SchedulerCustomizeResourcesControlsWpf {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+  
+            GenerateResources();
+        }
+
+        private void GenerateResources() {
+            scheduler.Storage.ResourceStorage.BeginUpdate();
+            for (int i = 0; i < 5; i++)
+            {
+                Resource resource = scheduler.Storage.CreateResource(i);
+                resource.Caption = "Resource" + i;
+                scheduler.Storage.ResourceStorage.Add(resource);
+            }
+            scheduler.Storage.ResourceStorage[1].SetColor(System.Windows.Media.Colors.Blue);
+            scheduler.Storage.ResourceStorage.EndUpdate();
+        }
+    }
+
+    public class ResourceColorConverter : MarkupExtension, IValueConverter {
+        static ResourceColorConverter instance = new ResourceColorConverter();
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return instance;
+        }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            Resource resource = (Resource)value;
+            System.Windows.Media.Color color = resource.GetColor();
+            System.Windows.Media.Color emptyColor = new System.Windows.Media.Color();
+            if (color == emptyColor)
+            {
+                SchedulerControl scheduler = (SchedulerControl)Application.Current.MainWindow.FindName("scheduler");
+                SchedulerColorSchemaCollection colorSchemas = scheduler.GetResourceColorSchemasCopy();
+                int resIndex = scheduler.Storage.ResourceStorage.Items.IndexOf(resource);
+
+                color = colorSchemas[resIndex % colorSchemas.Count].Cell;
+            }
+
+            SolidColorBrush brush = new SolidColorBrush(Color.FromArgb(color.A, color.R, color.G, color.B));
+
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ VB/MainWindow.xaml.vb
@@ -0,0 +1,66 @@
+﻿Imports System
+Imports System.Globalization
+Imports System.Windows
+Imports System.Windows.Data
+Imports System.Windows.Markup
+Imports System.Windows.Media
+Imports DevExpress.Xpf.Scheduler
+Imports DevExpress.XtraScheduler
+
+Namespace SchedulerCustomizeResourcesControlsWpf
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+
+            GenerateResources()
+        End Sub
+
+        Private Sub GenerateResources()
+            scheduler.Storage.ResourceStorage.BeginUpdate()
+            For i As Integer = 0 To 4
+                Dim resource As Resource = scheduler.Storage.CreateResource(i)
+                resource.Caption = "Resource" & i
+                scheduler.Storage.ResourceStorage.Add(resource)
+            Next i
+            scheduler.Storage.ResourceStorage(1).SetColor(System.Windows.Media.Colors.Blue)
+            scheduler.Storage.ResourceStorage.EndUpdate()
+        End Sub
+    End Class
+
+    Public Class ResourceColorConverter
+        Inherits MarkupExtension
+        Implements IValueConverter
+
+        Private Shared instance As New ResourceColorConverter()
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return instance
+        End Function
+
+        #Region "IValueConverter Members"
+
+        Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+            Dim resource As Resource = DirectCast(value, Resource)
+            Dim color As System.Windows.Media.Color = resource.GetColor()
+            Dim emptyColor As New System.Windows.Media.Color()
+            If color = emptyColor Then
+                Dim scheduler As SchedulerControl = DirectCast(Application.Current.MainWindow.FindName("scheduler"), SchedulerControl)
+                Dim colorSchemas As SchedulerColorSchemaCollection = scheduler.GetResourceColorSchemasCopy()
+                Dim resIndex As Integer = scheduler.Storage.ResourceStorage.Items.IndexOf(resource)
+
+                color = colorSchemas(resIndex Mod colorSchemas.Count).Cell
+            End If
+
+            Dim brush As New SolidColorBrush(System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B))
+
+            Return brush
+        End Function
+
+        Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Throw New NotImplementedException()
+        End Function
+
+        #End Region
+    End Class
+End Namespace
